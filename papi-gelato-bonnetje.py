4f83eedb76fa85602b126462258bc6ac334c8a17
AmarNagim <99069493@mydavinci.nl>
2021-10-02 14:41:26 +0200

    papi-gelato-bonnetje

---
+++ papi-gelato-bonnetje.py
@@ -0,0 +1,108 @@
+# papi gelato
+# Amar Nagim
+
+import time
+
+hornCount = 0
+boxCount = 0
+bolletjesCount = 0
+
+def icecreamShop():
+    global boxCount
+    global hornCount
+    global amountIcecream
+    global bolletjesCount
+    times = 1
+
+
+    print('Welkom bij Papi Gelato')
+    print('')
+    time.sleep(1)
+
+    amountIcecream = int(input('Hoeveel bolletjes wilt u?: '))
+
+    if amountIcecream >= 4 and amountIcecream <= 8:
+        print(f'Dan krijgt u van mij een bakje met {amountIcecream} bolletjes')
+        bolletjesCount += amountIcecream
+    while amountIcecream >8:
+        print('Zulke grote bakken hebben we niet')
+        print('')
+        amountIcecream = int(input('Hoeveel bolletjes wilt u?: ')) 
+
+    if amountIcecream >= 1 and amountIcecream <= 8:
+        bolletjesCount += amountIcecream
+        hornOrBox = input(f"""
+Wilt u deze {amountIcecream} bolletje(s) in een 
+A) hoorntje
+B) bakje? """).lower()
+        while hornOrBox not in ['a', 'b']:
+            print('')
+            print('Sorry, dat begreep ik niet..')
+            hornOrBox = input(f"""
+Wilt u deze {amountIcecream} bolletje(s) in een
+A) hoorntje
+B) bakje? """).lower()        
+        if hornOrBox == 'a':
+            hornCount += 1
+            hornOrBox = 'hoorntje'
+        elif hornOrBox == 'b':
+            boxCount += 1
+            hornOrBox = 'bakje'
+
+    times = 1
+    for flavour in range(amountIcecream):
+        print('')
+        flavourChoice = input(f'Welke smaak wilt u voor bolletje nummer {times}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?: ').lower()
+        while flavourChoice not in ['a', 'c', 'm', 'v']:
+            print('Sorry dat snap ik niet...')
+            flavourChoice = input(f'Welke smaak wilt u voor bolletje nummer {times}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?: ').lower()           
+        times += 1
+
+    global orderAgain
+    print('')
+    orderAgain = input(f'Hier is uw {hornOrBox} met {amountIcecream} bolletje(s). Wilt u nog meer bestellen? (Y/N): ').lower()     
+
+    while orderAgain not in ['y', 'n']:
+        print('Sorry dat snap ik niet..')    
+        orderAgain = input(f'Hier is uw {hornOrBox} met {amountIcecream} bolletje(s). Wilt u nog meer bestellen? (Y/N): ').lower()            
+
+    if orderAgain == 'n':
+        print('Bedankt voor uw bestelling, tot ziens!')    
+        time.sleep(2)
+        exit        
+icecreamShop()
+
+if orderAgain == 'y':
+    print('')
+    icecreamShop()
+
+priceAmountIceCream = format(bolletjesCount*1.10, ".2f")
+priceHornCount = format(hornCount*1.25, ".2f")
+priceBoxCount = format(boxCount*0.75, ".2f")
+
+print(priceAmountIceCream)
+
+print('     ---------------------------["Papi Gelato"]---------------------------')
+print('')
+if amountIcecream == 0:
+    print(f'      Horrentje(s)          {hornCount} x €1.25   = €{priceHornCount}')
+    print(f'      Bakje(s)              {boxCount} x €0.75   = €{priceBoxCount}')
+elif hornCount == 0:    
+    print(f'      Bolletje(s)           {bolletjesCount} x 1.10    = €{priceAmountIceCream}                                           ')
+    print(f'      Bakje(s)              {boxCount} x €0.75   = €{priceBoxCount}')
+    
+elif boxCount == 0:    
+    print(f'      Bolletje(s)           {bolletjesCount} x 1.10   = €{priceAmountIceCream}                                           ')
+    print(f'      Horrentje(s)          {hornCount} x €1.25   = €{priceHornCount}')
+else:
+    print(f'      Bolletje(s)           {bolletjesCount} x 1.10    = €{priceAmountIceCream}                                           ')
+    print(f'      Horrentje(s)          {hornCount} x €1.25   = €{priceHornCount}')
+    print(f'      Bakje(s)              {boxCount} x €0.75   = €{priceBoxCount}')
+
+priceAmountIceCreamFloat = float(priceAmountIceCream)
+priceHornCountFloat = float(priceHornCount)
+priceBoxCountFloat = float(priceBoxCount)
+
+total = format(priceAmountIceCreamFloat+priceHornCountFloat+priceBoxCountFloat, ".2f")
+print(f'      Totaal                            = €{total}                                  ')
+
